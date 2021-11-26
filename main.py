064cae9522a0e5151bfa24fb4a94bb0204bdc96a
Bharani <baranidharansuresh@gmail.com>
2021-11-26 11:25:57 +0530

    class 1

--- main.py
+++ main.py
@@ -1,16 +1,156 @@
-# This is a sample Python script.
+#comment and uncomment
+# ctrl+/
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# # print
+# print("barani")
+# print("barani  {0}".format(10))
+# print("python",100)
+#
+#
+# ##input
+#
+# a=int(input("enter the number"))
+# print(a)
+# print(type(a))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#varialbles
+# a
+# Aa
+# aA
+# A_1
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# A@
+# A$
+# 1A
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#keywords--reserverd words
+# for while if elif
+
+###operators
+# arthimetic --> +,-,*,/,%
+
+#relational --> >,<,>=,<=,==,!=
+
+#logical operator ->and ,or,not (&,|,!)
+
+#membership operator --> in not ,is is not
+
+
+# binary operator
+# Xor,and ,or,not,xand,left shift,right shift
+
+#assignment
+
+# a=10
+# b=20
+
+# a+=1-->a=a+1
+# a-=1-->a=a-1
+
+# int,char,float,string.list,tuple,set,dict
+
+#if
+#
+# a=100
+# if(a < 10):
+#     print(a)
+# else:
+#     print("a greater than 10")
+
+#elif
+
+# a=1
+# b=20
+# if(a == 1 and b ==2):
+#     print(a)
+# elif(a ==1):
+#     print(b)
+# else:
+#     print(a,b)
+
+#
+# print("p"  in "python" )
+# print("p"  not in "python" )
+
+
+
+#while
+# a=1
+# while(a < 10):
+#     print(a)
+#     a+=1
+
+
+#for
+
+# for i in range(10):
+#     print(i)
+#
+# for i in range(10,21):
+#     print(i)
+
+# for i in range(100,210,8):
+#     print(i)
+
+
+# for i in  range(100,2,-2):
+#     print(i)
+
+
+# a="python"
+# for i in a:
+#     print(i,end=",")
+
+#
+#
+# # list
+# a=[1,2,3,4,5]
+# a=[1,2,3,4,'5',6]
+#
+# # append
+# a.append(100)
+# print(a)
+#
+# a.insert(2,100)
+# print(a)
+#
+#
+# # indexing
+# print(a[0])
+# print(a[-4])
+#
+# #slicing
+# print(a[:3])
+# print(a[3:])
+# print(a[3:6])
+
+#extend
+# a=[12]
+# a.append([2345,78])
+# b=[10]
+# b.extend([1,4,78])
+# print(a)
+# print(b)
+
+
+#remove ,delete ,pop
+a=[1,2345,1]
+a.remove(2345)
+print(a)
+
+
+#pop
+a=[100,200,300]
+# a.pop(0)
+# print(a)
+
+#
+# del a[0]
+del a[1:3]
+print(a)
+
+a[1:3]=[100,300,400]
+print(a)
